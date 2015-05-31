af86e38560f16f70a0b74bcf2aeab4d855fbdc74
acemaster <vivekhtc25@gmail.com>
2015-05-31 23:40:52 +0530

    wsgi settings change

--- onestage_website/wsgi.py
+++ onestage_website/wsgi.py
@@ -6,11 +6,16 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
 import os
+import sys
 
+# assuming your django settings file is at '/home/onestagenitwweb/mysite/settings.py'
+path = '/home/onestagenitwweb/website'
+if path not in sys.path:
+   sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'onestage_website.settings'
+
+# then, for django >=1.5:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onestage_website.settings")
-
 application = get_wsgi_application()
